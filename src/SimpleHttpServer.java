eff987044e984d605d53b5d3db7e348fdfb3c83b
Jamie Wales <jamie.wales.developer@gmail.com>
2023-03-26 14:00:41 +0100

    Dealing with different URLs

--- src/SimpleHttpServer.java
+++ src/SimpleHttpServer.java
@@ -28,14 +28,30 @@ public class SimpleHttpServer {
         String method = requestParts[0];
         String path = requestParts[1];
 
-        String message = "<html><h1>Hello World!<h1></html>";
-        byte[] responseBytes = message.getBytes();
 
-        out.write("HTTP/1.1 200 OK\r\n".getBytes());
-        out.write("Content-type: text/html\r\n".getBytes());
-        out.write(("Content-Length: " + responseBytes.length + "\r\n").getBytes());
-        out.write("\r\n".getBytes());
-        out.write(responseBytes);
+        if ("/".equals(path)) {
+            String message = "<html><h1>Hello World!<h1></html>";
+            byte[] responseBytes = message.getBytes();
+
+            out.write("HTTP/1.1 200 OK\r\n".getBytes());
+            out.write("Content-type: text/html\r\n".getBytes());
+            out.write("Set-cookie: jamie=thebest\r\n".getBytes());
+            out.write(("Content-Length: " + responseBytes.length + "\r\n").getBytes());
+            out.write("\r\n".getBytes());
+            out.write(responseBytes);
+
+        } else {
+            String message = "<html><h1>Not found<h1></html>";
+            byte[] responseBytes = message.getBytes();
+
+            out.write("HTTP/1.1 404.0 Not Found\r\n".getBytes());
+            out.write("Content-type: text/html\r\n".getBytes());
+            out.write(("Content-Length: " + responseBytes.length + "\r\n").getBytes());
+            out.write("\r\n".getBytes());
+            out.write(responseBytes);
+
+        }
+
 
         out.close();
         in.close();
